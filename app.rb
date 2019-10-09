7a0630f59b800d0a34853abf415c690fad15be4d
Assane Savadogo <assane.savadogo81@gmail.com>
2019-10-08 21:13:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Assane"
+    end
+
+    get '/hometown' do 
+        "My hometown is Burkina faso"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Dispicable of Pharell William"
+    end
 end
